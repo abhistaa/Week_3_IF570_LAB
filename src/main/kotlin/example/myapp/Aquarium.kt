fa923bb639d69d254490ef8edc530793488cfeac
abhistaa <abhista.pramoda@student.umn.ac.id>
2023-03-02 18:46:58 +0700

    Creating a class

---
+++ src/main/kotlin/example/myapp/Aquarium.kt
@@ -0,0 +1,15 @@
+package example.myapp
+
+class Aquarium {
+    var width: Int = 20
+    var height: Int = 40
+    var lenght: Int = 100
+
+    fun printSize(){
+        println("Width: $width cm " + "Lenght: $lenght cm" + "Height: $height cm")
+    }
+
+}
+
+
+
