033d3833a0d707303fd05ec6a6abe4f38f0b1371
abhistaa <abhista.pramoda@student.umn.ac.id>
2023-03-02 19:37:18 +0700

    Learning about visibility modifiers

--- src/main/kotlin/example/myapp/Main.kt
+++ src/main/kotlin/example/myapp/Main.kt
@@ -2,10 +2,18 @@ package example.myapp
 
 fun buildAquarium() {
 
-    val aquarium6 = Aquarium(numberOfFish = 29)
-    aquarium6.printSize()
-    aquarium6.volume = 70
-    aquarium6.printSize()
+    val myAquarium = Aquarium(width = 25, length = 25, height = 40)
+    myAquarium.printSize()
+    val myTower = TowerTank(diameter = 25, height = 40)
+    myTower.printSize()
+
+//    val aquarium6 = Aquarium(length = 25, width = 25, height = 40)
+//    aquarium6.printSize()
+
+//    val aquarium6 = Aquarium(numberOfFish = 29)
+//    aquarium6.printSize()
+//    aquarium6.volume = 70
+//    aquarium6.printSize()
 
 //    val aquarium6 = Aquarium(numberOfFish = 29)
 //    aquarium6.printSize()
@@ -24,6 +32,7 @@ fun buildAquarium() {
 //    val aquarium4 = Aquarium(width = 25, height = 35, length = 110)
 //    aquarium4.printSize()
 }
+
 
 fun main(){
     buildAquarium()
